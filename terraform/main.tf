c6c184475b6be3cbe682566358b2b8dbdf0ed9d2
Dushyant Singh Chouhan <114542246+Dushyant-1@users.noreply.github.com>
2025-04-08 18:43:44 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,10 @@
 provider "azurerm" {
   features {}
+
+  client_id       = "0f687c5d-c508-4681-b598-37b5dbd610b4"
+  client_secret   = "U-O8Q~pTj-~m1LA6SZkuch71F-ePyLz1wEgLubi1"
   subscription_id = "1ecb68ad-b9f1-4f6e-8e63-c0e1d3d46c33"
+  tenant_id       = "c6deabb2-d214-4e9d-89f2-4013cd97071f"
 }
 
 resource "azurerm_resource_group" "this" {
