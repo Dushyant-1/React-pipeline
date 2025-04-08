d9b97473749969603e8434dbf38634661b242cd8
Dushyant Singh Chouhan <114542246+Dushyant-1@users.noreply.github.com>
2025-04-08 18:22:57 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,29 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "1ecb68ad-b9f1-4f6e-8e63-c0e1d3d46c33"
+}
+
+resource "azurerm_resource_group" "this" {
+  name     = "rg-jenkins"
+  location = var.location
+}
+
+resource "azurerm_service_plan" "this" {
+  name                = "appserviceplan-jenkins"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+  os_type             = "Linux"
+
+  sku_name = "B1"
+}
+
+resource "azurerm_linux_web_app" "this" {
+  name                = "Dushyant123"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+  service_plan_id     = azurerm_service_plan.this.id  # Updated attribute
+
+  site_config {
+    always_on = true
+  }
+}
